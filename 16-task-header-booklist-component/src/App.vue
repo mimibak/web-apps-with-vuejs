83a7267d1c2f88ddd77cf3c9664340dd29b3f1b8
Mimi Bak <bak.mihyeong@gmail.com>
2023-12-14 16:37:05 +0100

    feat:add-task-header-booklist-component

---
+++ 16-task-header-booklist-component/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <LayoutHeader />
+  <main>
+    <div id="app" class="inside">
+      <BookList />
+    </div>
+  </main>
+</template>
+
+<script>
+import LayoutHeader from "./components/LayoutHeader.vue";
+import BookList from "@/components/BookList.vue";
+
+export default {
+  name: "App",
+  components: {
+    LayoutHeader,
+    BookList,
+  },
+};
+</script>
