a05b616d7c3cf0f4feb6a0006aadf214c03ec5ad
Mimi Bak <bak.mihyeong@gmail.com>
2023-12-14 16:37:27 +0100

    feat:add-task-recap-livesession

---
+++ 17-task-recap/src/components/UserList.vue
@@ -0,0 +1,217 @@
+<template>
+  <section class="user-list">
+    <nav class="user-list__nav">
+      <p>{{ users.length }} Users</p>
+      <button @click="sortUsersBy('asc')" class="user-list__nav-filter">
+        Asc ⇧
+      </button>
+      <button
+        @click="sortUsersBy('desc')"
+        class="user-list__nav-filter user-list__nav-filter--active"
+      >
+        Desc ⇩
+      </button>
+      <a
+        href="#"
+        v-for="user in users"
+        :key="user.id"
+        class="user-list__nav-item"
+        @click.prevent="showUserDetails(user)"
+      >
+        {{ user.username }}</a
+      >
+    </nav>
+    <!-- Add Detail component here -->
+    <UserListDetail v-if="selectedUser" :users="selectedUser" />
+    <p v-else class="user-list__details-hint">
+      Es wurde kein Nutzer gefunden / ausgewählt
+    </p>
+  </section>
+</template>
+
+<script>
+import UserListDetail from "@/components/UserListDetail.vue";
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Leanne Graham",
+          username: "Bret",
+          email: "",
+          phone: "1-770-736-8031 x56442",
+          company: {
+            name: "Romaguera-Crona",
+          },
+        },
+        {
+          id: 2,
+          name: "Ervin Howell",
+          username: "Antonette",
+          email: "Shanna@melissa.tv",
+          phone: "010-692-6593 x09125",
+          company: {
+            name: "Deckow-Crist",
+          },
+        },
+        {
+          id: 3,
+          name: "Clementine Bauch",
+          username: "Samantha",
+          email: "Nathan@yesenia.net",
+          phone: "1-463-123-4447",
+          company: {
+            name: "Romaguera-Jacobson",
+          },
+        },
+        {
+          id: 4,
+          name: "Patricia Lebsack",
+          username: "Karianne",
+          email: "Julianne.OConner@kory.org",
+          phone: "493-170-9623 x156",
+          company: {
+            name: "Robel-Corkery",
+            catchPhrase: "Multi-tiered zero tolerance productivity",
+            bs: "transition cutting-edge web services",
+          },
+        },
+        {
+          id: 5,
+          name: "Chelsey Dietrich",
+          username: "Kamren",
+          email: "Lucio_Hettinger@annie.ca",
+          phone: "(254)954-1289",
+          company: {
+            name: "Keebler LLC",
+          },
+        },
+        {
+          id: 6,
+          name: "Mrs. Dennis Schulist",
+          username: "Leopoldo_Corkery",
+          email: "Karley_Dach@jasper.info",
+          phone: "1-477-935-8478 x6430",
+          company: {
+            name: "Considine-Lockman",
+          },
+        },
+        {
+          id: 7,
+          name: "Kurtis Weissnat",
+          username: "Elwyn.Skiles",
+          email: "Telly.Hoeger@billy.biz",
+          phone: "",
+          company: {
+            name: "Johns Group",
+          },
+        },
+        {
+          id: 8,
+          name: "Nicholas Runolfsdottir V",
+          username: "Maxime_Nienow",
+          email: "Sherwood@rosamond.me",
+          phone: "586.493.6943 x140",
+          company: {
+            name: "Abernathy Group",
+          },
+        },
+        {
+          id: 9,
+          name: "Glenna Reichert",
+          username: "Delphine",
+          email: "Chaim_McDermott@dana.io",
+          phone: "(775)976-6794 x41206",
+          company: {
+            name: "",
+          },
+        },
+        {
+          id: 10,
+          name: "Clementina DuBuque",
+          username: "Moriah.Stanton",
+          email: "Rey.Padberg@karina.biz",
+          phone: "024-648-3804",
+          company: {
+            name: "Hoeger LLC",
+          },
+        },
+      ],
+      selectedUser: null,
+    };
+  },
+  created() {
+    this.sortUsersBy("desc");
+  },
+  components: {
+    UserListDetail,
+  },
+  methods: {
+    showUserDetails(user) {
+      this.selectedUser = user;
+    },
+    sortUsersBy(order) {
+      this.users.sort((a, b) => {
+        if (order === "asc") {
+          return a.username.localeCompare(b.username);
+        } else if (order === "desc") {
+          return b.username.localeCompare(a.username);
+        }
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+.user-list {
+  font-family: Arial, Helvetica, sans-serif;
+  display: flex;
+  width: 80%;
+  margin: 0 auto;
+  border: 3px solid #00a8ff;
+  border-radius: 5px;
+}
+.user-list__nav {
+  width: 40%;
+  padding: 1rem;
+}
+.user-list__nav-filter {
+  text-transform: uppercase;
+  font-size: 0.7rem;
+  line-height: 1;
+  color: #00a8ff;
+  border: 2px solid #00a8ff;
+  background-color: transparent;
+  padding: 2px;
+  margin-right: 5px;
+  margin-bottom: 15px;
+  cursor: pointer;
+}
+.user-list__nav-filter--active {
+  color: #fff;
+  border-color: transparent;
+  background-color: #00a8ff;
+}
+.user-list__nav-item {
+  display: block;
+  text-decoration: none;
+  color: #00a8ff;
+  padding: 5px;
+  border: 2px solid transparent;
+  border-radius: 3px;
+  margin-bottom: 2px;
+}
+.user-list__nav-item:hover {
+  border-color: #00a8ff;
+}
+.user-list__nav-item--active {
+  background-color: #00a8ff;
+  color: #fff;
+  border-color: transparent;
+}
+</style>
---
+++ 17-task-recap/src/components/UserListDetail.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="user-list__details">
+    <h2 class="user-list__details-hl">Details</h2>
+    <dl v-if="users">
+      <dt>name:</dt>
+      <dd>{{ users.name || "-" }}</dd>
+
+      <dt>email:</dt>
+      <dd>{{ users.email || "-" }}</dd>
+
+      <dt>phone:</dt>
+      <dd>{{ users.phone || "-" }}</dd>
+
+      <dt>company:</dt>
+      <dd>{{ users.company.name || "-" }}</dd>
+    </dl>
+    <p v-else class="user-list__details-hint">
+      Es wurde kein Nutzer gefunden / ausgewählt
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    users: Object,
+  },
+};
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+.user-list__details {
+  width: 60%;
+  padding: 1rem;
+  color: #192a56;
+}
+.user-list__details-hl {
+  color: #00a8ff;
+}
+.user-list__details dd {
+  margin-bottom: 1rem;
+}
+.user-list__details-hint {
+  border: 2px solid #e84118;
+  border-radius: 5px;
+  color: #e84118;
+  padding: 0.5rem;
+}
+</style>
